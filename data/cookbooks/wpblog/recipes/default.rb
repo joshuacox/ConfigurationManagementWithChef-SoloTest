aa7fb9fbc0d343979797b25fdbc1826c083bdd63
Josh Cox <josh@webhosting.coop>
2014-10-15 13:23:36 -0500

    attributes

--- data/cookbooks/wpblog/recipes/default.rb
+++ data/cookbooks/wpblog/recipes/default.rb
@@ -7,3 +7,38 @@ include_recipe "php"
 include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
 # include_recipe "mysql::ruby"
+%w{
+    curl
+    vim
+    screen
+	libmysqlclient-dev
+}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+apache_site "default" do
+  enable true
+end
+
+mysql_database node['wpblog']['database'] do
+  connection({
+    :host     => 'localhost',
+    :username => 'root',
+    :password => node['mysql']['server_root_password']
+  })
+  action :create
+end
+
+mysql_database_user node['wpblog']['db_username'] do
+  connection ({
+  	:host => 'localhost',
+  	:username => 'root',
+  	:password => node['mysql']['server_root_password']
+  })
+  password node['wpblog']['db_password']
+  database_name node['wpblog']['database']
+  privileges [:select, :update, :insert, :create, :delete]
+  action :grant
+end
