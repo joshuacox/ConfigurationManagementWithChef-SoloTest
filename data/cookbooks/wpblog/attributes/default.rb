2ce82d40f43a97463f29c27df6b0d1d393401fd9
Josh Cox <josh@webhosting.coop>
2014-10-16 12:24:22 -0500

    default attributes

--- data/cookbooks/wpblog/attributes/default.rb
+++ data/cookbooks/wpblog/attributes/default.rb
@@ -3,3 +3,5 @@ default.apache.listen_ports = [80, 443]
 default.wpblog.database = 'wpblog'
 default.wpblog.db_username = 'wpblog'
 default.wpblog.db_password = 'random_password'
+default.wpblog.path = '/var/www/wpblog'
+default.wpblog.server_name = 'wpblog'
